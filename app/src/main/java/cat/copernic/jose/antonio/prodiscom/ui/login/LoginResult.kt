b4c3a003b517bf2aa75a66a63a7dde665652558a
Jose Antonio Miranda <miranda.segura.jose.antonio@alumnat.copernic.cat>
2021-10-26 12:29:05 +0200

    Plantilla Login Creada.

---
+++ app/src/main/java/cat/copernic/jose/antonio/prodiscom/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package cat.copernic.jose.antonio.prodiscom.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
