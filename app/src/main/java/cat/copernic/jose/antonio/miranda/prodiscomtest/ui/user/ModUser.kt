db25c3f915bd38ca9267672be6dac7e34a4ca4b2
Jose Antonio Miranda <miranda.segura.jose.antonio@alumnat.copernic.cat>
2021-11-22 22:38:32 +0100

    UpdateUser Functional

--- app/src/main/java/cat/copernic/jose/antonio/miranda/prodiscomtest/ui/user/ModUser.kt
+++ app/src/main/java/cat/copernic/jose/antonio/miranda/prodiscomtest/ui/user/ModUser.kt
@@ -1,26 +1,119 @@
 package cat.copernic.jose.antonio.miranda.prodiscomtest.ui.user
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.IntegerRes
 import androidx.navigation.Navigation
 import cat.copernic.jose.antonio.miranda.prodiscomtest.R
 import cat.copernic.jose.antonio.miranda.prodiscomtest.databinding.FragmentModUserBinding
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class modUser : Fragment() {
     private var _binding: FragmentModUserBinding? = null
     private val binding get() = _binding!!
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var getUserInfo: DocumentReference
+    private var found = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentModUserBinding.inflate(inflater, container, false)
 
-        binding.btnReturnModUser.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.usuarios, null))
-        binding.btnModUserToHome.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.menu_principal, null))
+        binding.btnReturnModUser.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.usuarios,
+                null
+            )
+        )
+        binding.btnModUserToHome.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.menu_principal,
+                null
+            )
+        )
 
+        binding.iVBuscar.setOnClickListener {
+            if (binding.eTxtSearch.text.toString().isNotEmpty()) {
+                getInfo(binding.eTxtSearch.text.toString())
+            } else {
+                notFoundError()
+            }
+        }
+
+        binding.btnModUs.setOnClickListener{
+            confirmUpdate()
+        }
         return binding.root
     }
+
+    //TODO USAR LA FUNCION DE COMPROBACION PARA DNI Y MAIL PARA BUSCAR POR LOS DOS CAMPOS
+    //TODO PASAR A MVVM
+
+    private fun getInfo(mailUser: String): Boolean {
+        found = false
+        getUserInfo = db.collection("users").document(mailUser)
+        getUserInfo.get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    printInfo(
+                        document.get("email") as String,
+                        document.get("Nombre") as String,
+                        document.get("DNI") as String,
+//                        document.get("Tel") as String
+                    )
+                    found = true
+                } else {
+                    notFoundError()
+                }
+            }
+        return found
+    }
+
+
+    private fun printInfo(mail: String, nom: String, dni: String /*tel: String*/) {
+
+        binding.txResultMail.setText(mail)
+        binding.txResultNom.setText(nom)
+        binding.txResultDni.setText(dni)
+//        binding.txResultTel.setText(tel)
+
+    }
+
+    private fun notFoundError() {
+        val errorDis = AlertDialog.Builder(activity)
+        errorDis.setTitle("Usuari no trovat")
+        errorDis.setMessage("No existeix cap usuari amb aquestes dades")
+        errorDis.setPositiveButton("Aceptar", null)
+        errorDis.show()
+    }
+
+
+    private fun updateUser() {
+        getUserInfo.update(
+            "email",
+            binding.txResultMail.text.toString(),
+            "DNI",
+            binding.txResultDni.text.toString(),
+            "Nombre", binding.txResultNom.text.toString()
+        )
+    }
+
+    private val positiveButtonClick = {dialog: DialogInterface, which: Int -> updateUser()}
+
+    private fun confirmUpdate(){
+        val updateDis = AlertDialog.Builder(activity)
+        updateDis.setTitle("Modificar Usuari")
+        updateDis.setMessage("Estas segur que vols modificar el missatge?")
+        updateDis.setPositiveButton("Aceptar", DialogInterface.OnClickListener(function = positiveButtonClick))
+        updateDis.setNegativeButton("Cancelar", null)
+        updateDis.show()
+    }
 }
+
